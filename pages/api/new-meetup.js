53209401f4e1bb66a7c62c41093ad36ab842b3d7
vsheva <valera.sheva@gmail.com>
2022-10-17 01:48:52 +0300

    Add Mongodb,  backend

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,12 +1,32 @@
-// POST api/new-meetup
+import {MongoClient} from "mongodb";
 
+// POST api/new-meetup
 //in -> req, res object
 
-function handler(req, res) {
+//1. Getting Request
+
+async function handler(req, res) {
     if(req.method ==="POST") {
         const data= req.body;
 
         const {image, title, address, description}= data;
+
+        //2. storing data in the db
+
+       const client = await MongoClient.connect("mongodb+srv://shev1181:a12850000@cluster0.pekmzk1.mongodb.net/meetups?retryWrites=true&w=majority");
+       const db= client.db();
+
+       const meetupsCollection = db.collection("meetups");
+
+       const result = await meetupsCollection.insertOne(data);
+
+       console.log(result)
+
+        client.close();
+
+       //3.send back the response
+
+        res.status(201).json({message:"Meetup inserted!"});
     }
 }
 
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -5,7 +5,17 @@ import React from "react";
 
 
 const NewMeetupPage = () => {
-    const addMeetupHandler=(enteredMeetupData)=>console.log(enteredMeetupData);
+
+    const addMeetupHandler= async(enteredMeetupData)=>{
+        const response = await fetch("/api/new-meetup", {
+            method: 'POST',
+            body:JSON.stringify(enteredMeetupData),
+            headers: {'Content-Type': 'application/json'}
+        })
+        const data = await response.json()
+
+        console.log(data)
+    }
 
     return  <NewMeetupForm  onAddMeetup={addMeetupHandler}/>
 }
