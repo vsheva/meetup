482e23b12177537ad8cb413c50f498725cc8ddea
vsheva <valera.sheva@gmail.com>
2022-10-18 01:33:15 +0300

    Fix callback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -34,7 +34,7 @@ export const getStaticPaths = async () => {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking", //or true
     paths: meetups.map(meetup => ({ params: { meetupId: meetup._id.toString() } })),
   };
 };
